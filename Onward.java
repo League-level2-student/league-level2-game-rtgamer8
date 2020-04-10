f5cfeaf8aade28bef946c8b453a6c1b7a5e3e0cd
root <root@f3ac95792646>
2020-04-10 20:29:29 +0000

    started my own game, but before that I finished up game draft in my notebook.

---
+++ Onward.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Onward {
+public static void main(String[] args) {
+	ss
+	JFrame frame = new JFrame(); 
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.addAncestorListener(this);
+	int score = 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
