9e70a260fdb5b57ad65772f7d7fe4e6f05fab078
root <root@89022c915240>
2020-04-14 18:23:33 +0000

    Worked on my game

--- Onward.java
+++ Onward.java
@@ -1,3 +1,4 @@
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -9,18 +10,23 @@ public static void main(String[] args) {
 	JPanel panel = new JPanel();
 	JButton button = new JButton();
 	frame.setVisible(true);
-	frame.add(panel);
-	panel.addAncestorListener(this);
+	frame.add(panel);ner(this);
 	int score = 0;
-	
+	int score +=6;
+	JLabel label = new JLabel();
+	JCheckBox = new JCheckBox();
 	for (int i = 0; i < 10; i++) {
 		System.out.println("this is running " + i + "amount of times");
 	}
+
+	int area = i;
 	
-	
-	
-	
-	
+	if (area = 0 ) {
+		for (int j = 0; j < 1000000000000000000000000000000; j++) {
+			
+		}
+		System.out.println("you have covered " +i+ " meters of gorund");
+	}
 	
 	
 	
