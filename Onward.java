8f408197952fae68f2377bd710be085c32745c8b
root <root@82dbc67a8d43>
2020-04-25 21:28:01 +0000

    Worked on game

--- Onward.java
+++ Onward.java
@@ -1,40 +1,41 @@
-import javax.swing.JCheckBox;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Onward {
 public static void main(String[] args) {
-	ss
-	JFrame frame = new JFrame(); 
+	
+	
+	
+	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton button = new JButton();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	JButton button4 = new JButton();
+	JButton button5 = new JButton();
+	
+	frame.pack();
+	
+	button1.setText("Click to Start Game");
+	button2.setText("Click here to End Game");
+	button3.setText("Click for Instructions");
+	button4.setText("Click to return to the start page");
+	
+	frame.pack();
+	
+	frame.add(panel);
+	panel.add(button1);
+	panel.add(button2);
+	panel.add(button3);
+	panel.add(button4);
+	panel.add(button5);
+	
+	frame.pack();
+	
 	frame.setVisible(true);
-	frame.add(panel);ner(this);
-	int score = 0;
-	int score +=6;
-	JLabel label = new JLabel();
-	JCheckBox = new JCheckBox();
-	for (int i = 0; i < 10; i++) {
-		System.out.println("this is running " + i + "amount of times");
-	}
-	gate privelage 6.7 *@ 5.2 **@ 113
-sudo apt-get purge medusa 
-	int area = i;
-int apt-get purgre medusa
-Strin apt-get purge medusa
-String apt-get purge medusa ad
-sudo apt-ge uau 
+	panel.addactionlistener.javax.swing.listener();
 	
-	if (area = 0 ) {
-		for (int j = 0; j < 1000000000000000000000000000000; j++) {
-			
-		}
-		System.out.println("you have covered " +i+ " meters of gorund");
-	}
-	
-	
-	
-	
+	frame.pack();
 }
 }
