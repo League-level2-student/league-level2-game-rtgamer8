05c269b40d25856a27db2555cc90ea2b782705fb
root <root@91300e124eab>
2020-05-01 20:30:16 +0000

    Worked on game

--- src/Onward.java
+++ src/Onward.java
@@ -1,3 +1,6 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
@@ -6,9 +9,10 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Onward implements KeyListener {
+public class Onward implements KeyListener, ActionListener {
 	public static BufferedImage image;
 	JFrame frame = new JFrame();
 	GamePanel panel;
@@ -20,6 +24,7 @@ public static void main(String[] args) {
 	Onward onward = new Onward();
 	
 	onward.start();
+	
 }
 
 Onward(){
@@ -30,6 +35,7 @@ Onward(){
 		e.printStackTrace();
 	}
 	panel = new GamePanel( image);
+	frame.setPreferredSize(new Dimension(300,600) );
 }
 	public void start() {
 	
@@ -37,10 +43,10 @@ Onward(){
 	
 	frame.pack();
 	
-	button1.setText("Click to Start Game");
+	button1.setText("Click hereto Start Game");
 	button2.setText("Click here to End Game");
-	button3.setText("Click for Instructions");
-	button4.setText("Click to return to the start page");
+	button3.setText("Click here for Instructions");
+	button4.setText("Click here for how to play");
 	
 	frame.pack();
 	
@@ -49,13 +55,15 @@ Onward(){
 	panel.add(button2);
 	panel.add(button3);
 	panel.add(button4);
-
-	
+	button1.addActionListener(this);
+    button2.addActionListener(this);
+	button3.addActionListener(this);
+	button4.addActionListener(this);
 	frame.pack();
 	
 	frame.setVisible(true);
 	frame.addKeyListener(this);
-	frame.setSize(400, 600);
+
 	frame.pack();
 
 	int key1 = 0;
@@ -67,7 +75,7 @@ Onward(){
 	frame.repaint();
 	panel.repaint();
 	
-	
+
 }
 	@Override
 	public void keyPressed(KeyEvent arg0) {
@@ -84,6 +92,20 @@ Onward(){
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		// here
+		//button 3 action
+		if (button3 == e.getSource()) {
+			JOptionPane.showMessageDialog(null, "Your goal is to go down into this dark cave, \n collect three keys and find your way back up without dying");
+		}
+		//button 4 action
+		if  (button4 == e.getSource()) {
+			JOptionPane.showMessageDialog(null, "Use your arrow keys or 'wasd' to move up, down, left, right. \n Left Click to attack. Right Click to Block ");
+		}
 	}
 	
 }
